434cadc8929b9b365a6f1c1f697300b782a834f8
Benjamin Chen <yeehenchen@gmail.com>
2018-11-12 12:27:50 +0800

    move the logical layer into controller instead of view

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,5 +5,12 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params["question"]
+    if @question == "I am going to work"
+      @result = "Great!"
+    elsif @question.split(//).include? "?"
+      @result = "Silly question, get dressed and go to work!"
+    else
+      @result = "I don't care, get dressed and go to work!"
+    end
   end
 end
